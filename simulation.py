9d31398e2e3f7b9560ba0080b3bd6f7956b2bc27
LIsabelleQrz <84094874+LIsabelleQrz@users.noreply.github.com>
2023-01-07 22:00:27 -0400

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -10,12 +10,22 @@ class Simulation:
         self.balls = []
         self.colors = [(0,0,255), (255,255,0), (255,0,0), (0,128,0)]
         self.createBalls()
+        
+    def checkPosition(self, radius, x, y):
+        for ball in self.balls:
+            distance = math.sqrt((ball.x - x)**2 + (ball.y - y)**2)
+            if distance < ball.radius + radius:
+                return False
+        return True
 
     def createBalls(self):
         for _ in range(self.numBalls):
             radius = rd.randint(10, 50)
-            x = rd.randint(radius, self.screenSize - radius)
-            y = rd.randint(radius, self.screenSize - radius)
+            while(True):
+                x = rd.randint(radius, self.screenSize - radius)
+                y = rd.randint(radius, self.screenSize - radius)
+                if(self.checkPosition(radius, x, y)):
+                    break
             speed_x = speed_y = 0
             while speed_x == 0 and speed_y == 0:
                 speed_x = rd.randint(0, 10)
@@ -33,7 +43,7 @@ class Simulation:
     def checkCollisions(self):
         for i in range(self.numBalls-1):
             for j in range(i+1, self.numBalls):
-                self.balls[i].checkCollision(self.balls[j])
+                self.balls[i].checkCollision(self.balls[j], self.screenSize)
 
     def checkBounds(self):
         for ball in self.balls:
